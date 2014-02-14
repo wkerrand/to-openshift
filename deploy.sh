b307c292d67135c8c6d33da12e1ba4ef1437be33
wkerrand <wenceslask@hotmail.fr>
2014-02-14 12:54:25 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,22 @@
-"hello"
+#!/bin/bash
+prompt="Pick an option:"
+options=("Clone" "Commit" "Push")
+ echo "$title"
+ PS3="$prompt"
+ select opt in "${options[@]}" 
+"Quit"; do
+ case $n in 
+1)
+ # Specifier l'url et le repertoire du projet Openshift 
+# git clone <git_url> <directory to create>
+ ;;
+ 2) git add $1 git
+ commit -m "Commit..";;
+ 3) git push;; 
+4) $(( ${#options[@]}+1 ))
+ echo "Goodbye!"; break;;
+ *) echo "Invalid option. 
+Try another one.";continue;;
+ esac 
+Done
+
